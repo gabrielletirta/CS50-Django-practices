2c10f00c9b07f1e040045481ce4aaed7bcf326ea
Gabrielle Tirta <tirtagabrielle@gmail.com>
2023-09-23 16:48:02 +1000

    Added newyear page and added logic to it

--- newyear/urls.py
+++ newyear/urls.py
@@ -1,7 +1,10 @@
+# This line imports the 'path' function from Django 'urls' module. 'path' function is used to define URL patterns.
 from django.urls import path 
 
+#this line imports the 'views' module from the current directory . The (.) represents the current directory
 from . import views
 
+# urlpatterns is a configuration variables in Django used to define url attern and the associated view function or class that should handle requests matching the pattern.
 urlpatterns = [
-    path(",views.index, ")
+    path("", views.index, name="index")
 ]
